c89e42b66e63b4d5db54fa1b5a03e5ec3d317ee5
Glenroy Logan <glogan@griddynamics.com>
2023-09-11 01:29:43 -0500

    Allure Report Demo 1
    Allure Report Demo 1

---
+++ AllureReportDemo/src/test/java/com/sdetadda/allure/Testcase3.java
@@ -0,0 +1,19 @@
+package com.sdetadda.allure;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class Testcase3 {
+
+	@Test
+	public void TestMethod3() {
+		throw new SkipException("TestMethod3 will be skipped");
+	}
+
+	@Test
+	public void TestMethod4() {
+		throw new SkipException("TestMethod4 will be skipped");
+	}
+	
+}
