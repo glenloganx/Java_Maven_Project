5803dace59e24b47d63a96f9bc335df727f63324
glenloganx <glogan@griddynamics.com>
2023-09-13 15:16:33 -0500

    Allure Report Demo 2
    Allure Report Demo 2

---
+++  AllureReportDemo2-IntelliJ /src/main/java/com/sdetadda/allure/Testcase2.java
@@ -0,0 +1,14 @@
+package com.sdetadda.allure;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Testcase2 {
+
+    @Test
+    public void TestMethod2() {
+        Assert.assertTrue(true);
+        System.out.println("TestMethod2 is working as expected");
+    }
+
+}
